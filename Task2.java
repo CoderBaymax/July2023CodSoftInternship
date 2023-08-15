439a085990eba7dcdc232f507e3bb11e51e117f9
Meera N <118354738+CoderBaymax@users.noreply.github.com>
2023-08-15 20:10:16 +0530

    Update and rename WordCount.java to Task2.java

--- Task2.java
+++ Task2.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class WordCount {
+public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputText="";
